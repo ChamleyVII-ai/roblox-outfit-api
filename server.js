6e9a7098b62524ddd0f98a9f7af27d1552cb6040
ChamleyVII-ai <ChamleyVII@hotmail.com>
2026-06-14 13:12:17 +0200

    Update server.js

--- server.js
+++ server.js
@@ -12,11 +12,40 @@ app.get("/outfits/:userId", async (req, res) => {
         const userId = req.params.userId;
 
         const response = await fetch(
-            `https://avatar.roblox.com/v2/avatar/users/${userId}/outfits?itemsPerPage=50`
+            `https://avatar.roblox.com/v2/avatar/users/${userId}/outfits?itemsPerPage=100&isEditable=true`
         );
 
         const data = await response.json();
-        res.json(data);
+
+        const outfits = (data.data || []).filter(outfit =>
+            outfit.outfitType === "Avatar" && outfit.isEditable === true
+        );
+
+        const ids = outfits.map(outfit => outfit.id).join(",");
+
+        let thumbnails = {};
+
+        if (ids.length > 0) {
+            const thumbResponse = await fetch(
+                `https://thumbnails.roblox.com/v1/users/outfits?userOutfitIds=${ids}&size=150x150&format=Png&isCircular=false`
+            );
+
+            const thumbData = await thumbResponse.json();
+
+            for (const thumb of thumbData.data || []) {
+                thumbnails[thumb.targetId] = thumb.imageUrl;
+            }
+        }
+
+        const finalOutfits = outfits.map(outfit => ({
+            id: outfit.id,
+            name: outfit.name,
+            image: thumbnails[outfit.id] || ""
+        }));
+
+        res.json({
+            data: finalOutfits
+        });
 
     } catch (err) {
         res.status(500).json({
