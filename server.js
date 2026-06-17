fca3624939f55adc468d5a945a1c03fb6f9b1cf7
ChamleyVII-ai <ChamleyVII@hotmail.com>
2026-06-17 02:51:22 +0200

    server.js

--- server.js
+++ server.js
@@ -11,41 +11,38 @@ app.get("/outfits/:userId", async (req, res) => {
     try {
         const userId = req.params.userId;
 
-        const response = await fetch(
-            `https://avatar.roblox.com/v2/avatar/users/${userId}/outfits?itemsPerPage=100&isEditable=true`
-        );
+       const response = await fetch(
+    `https://avatar.roblox.com/v2/avatar/users/${userId}/outfits?itemsPerPage=100&isEditable=true`
+);
 
-        const data = await response.json();
+const data = await response.json();
 
-        const outfits = (data.data || []).filter(outfit =>
-            outfit.outfitType === "Avatar" && outfit.isEditable === true
-        );
+const outfits = (data.data || []).filter(outfit =>
+    outfit.outfitType === "Avatar" &&
+    outfit.isEditable === true
+);
 
-        const ids = outfits.map(outfit => outfit.id).join(",");
+const ids = outfits.map(outfit => outfit.id).join(",");
 
-        let thumbnails = {};
+let thumbnails = {};
 
-        if (ids.length > 0) {
-            const thumbResponse = await fetch(
-                `https://thumbnails.roblox.com/v1/users/outfits?userOutfitIds=${ids}&size=150x150&format=Png&isCircular=false`
-            );
+if (ids.length > 0) {
+    const thumbResponse = await fetch(
+        `https://thumbnails.roblox.com/v1/users/outfits?userOutfitIds=${ids}&size=150x150&format=Png&isCircular=false`
+    );
 
-            const thumbData = await thumbResponse.json();
+    const thumbData = await thumbResponse.json();
 
-            for (const thumb of thumbData.data || []) {
-                thumbnails[thumb.targetId] = thumb.imageUrl;
-            }
-        }
+    for (const thumb of thumbData.data || []) {
+        thumbnails[thumb.targetId] = thumb.imageUrl;
+    }
+}
 
-        const finalOutfits = outfits.map(outfit => ({
-            id: outfit.id,
-            name: outfit.name,
-            image: thumbnails[outfit.id] || ""
-        }));
-
-        res.json({
-            data: finalOutfits
-        });
+const finalOutfits = outfits.map(outfit => ({
+    id: outfit.id,
+    name: outfit.name,
+    image: thumbnails[outfit.id] || ""
+}));
 
     } catch (err) {
         res.status(500).json({
